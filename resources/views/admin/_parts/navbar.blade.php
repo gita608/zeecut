7804a5d9e54915ce2a66d18f9e8ec341b4946453
rabil <rabilmohammed007@gmail.com>
2025-03-12 09:41:01 +0530

    product & Category menu Icons

--- resources/views/admin/_parts/navbar.blade.php
+++ resources/views/admin/_parts/navbar.blade.php
@@ -2,9 +2,9 @@
 <nav class="sidebar">
   <div class="sidebar-header">
     @if(Route::has('admin.dashboard'))
-      <a href="{{ route('admin.dashboard') }}" class="sidebar-brand">
-        {{ env('APP_NAME') }}
-      </a>
+    <a href="{{ route('admin.dashboard') }}" class="sidebar-brand">
+      {{ env('APP_NAME') }}
+    </a>
     @endif
     <div class="sidebar-toggler not-active">
       <span></span>
@@ -17,12 +17,12 @@
       <li class="nav-item nav-category">Menu</li>
 
       @if(Route::has('admin.dashboard'))
-        <li class="nav-item">
-          <a href="{{ route('admin.dashboard') }}" class="nav-link">
-            <i class="link-icon" data-feather="box"></i>
-            <span class="link-title">Dashboard</span>
-          </a>
-        </li>
+      <li class="nav-item">
+        <a href="{{ route('admin.dashboard') }}" class="nav-link">
+          <i class="link-icon" data-feather="box"></i>
+          <span class="link-title">Dashboard</span>
+        </a>
+      </li>
       @endif
 
       <li class="nav-item d-none">
@@ -48,20 +48,35 @@
       </li>
 
       @if(Route::has('user.index'))
-        <li class="nav-item">
-          <a href="{{ route('user.index') }}" class="nav-link">
-            <i class="link-icon" data-feather="users"></i>
-            <span class="link-title">Users</span>
-          </a>
-        </li>
-      @endif
-
       <li class="nav-item">
-        <a href="#" class="nav-link">
-          <i class="link-icon" data-feather="calendar"></i>
-          <span class="link-title">Calendar</span>
+        <a href="{{ route('user.index') }}" class="nav-link">
+          <i class="link-icon" data-feather="users"></i>
+          <span class="link-title">Users</span>
         </a>
       </li>
+      @endif
+
+      @if(Route::has('category.index'))
+      <li class="nav-item">
+        <a href="{{ route('category.index') }}" class="nav-link">
+          <i class="link-icon" data-feather="grid"></i> {{-- Category Icon --}}
+          <span class="link-title">Category</span>
+        </a>
+      </li>
+      @endif
+
+      @if(Route::has('product.index'))
+      <li class="nav-item">
+        <a href="{{ route('product.index') }}" class="nav-link">
+          <i class="link-icon" data-feather="box"></i> {{-- Product Icon --}}
+          <span class="link-title">Product</span>
+        </a>
+      </li>
+      @endif
+
+
+
+
     </ul>
   </div>
 </nav>
