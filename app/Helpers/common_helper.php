f75a5ad51fa0cbe8d45909164548c1de8f21183d
Ameer Suhail <ameersuhail2001@gmail.com>
2025-04-28 23:59:15 +0530

    common helper

--- app/Helpers/common_helper.php
+++ app/Helpers/common_helper.php
@@ -6,4 +6,27 @@ if (!function_exists('format_price')) {
     {
         return '₹ ' . number_format($amount);
     }
+
+    if (!function_exists('get_setting')) {
+        function get_setting($key, $default = null)
+        {
+            $setting = cache()->remember("setting_{$key}", 60*60, function () use ($key) {
+                return Setting::where('key', $key)->value('value');
+            });
+    
+            return $setting ?? $default;
+        }
+    }
+
+    function set_setting($key, $value)
+    {
+        $setting = Setting::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
+
+        cache()->forget("setting_{$key}");
+
+        return $setting;
+    }
 }
