47673eda339e7a541b64317dd2eb2c70e472a82d
Ameer Suhail <ameersuhail2001@gmail.com>
2025-05-09 22:54:58 +0530

    Stock model and stock page

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -3,33 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
- use App\Models\OrderItem;
+use App\Models\OrderItem;
+
 class Stock extends Model
 {
-    protected $fillable = ['user_id','product_id','quantity'];
+    protected $fillable = ['user_id', 'product_id', 'quantity'];
 
-     // Relationship: Stock belongs to a Product
-     public function product()
-     {
-         return $this->belongsTo(Product::class);
-     }
+    // Relationship: Stock belongs to a Product
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 
 
-    public function get_product_stock($product_id){
+    public function get_product_stock($product_id)
+    {
 
-        $out_of_quantity = Stock::where('product_id',$product_id)->first()->quantity;
+        $out_of_quantity = Stock::where('product_id', $product_id)->first()->quantity;
         $consumed_stocks = OrderItem::where('product_id', $product_id)->sum('quantity');
 
         return round($out_of_quantity - $consumed_stocks, 2);
-     }
+    }
 
-    public function get_user_cart_stock_check($cart_quantity, $product_id, $request_quantity) {
+    public function get_user_cart_stock_check($cart_quantity, $product_id, $request_quantity)
+    {
 
         $remaining_stock = $this->get_product_stock($product_id);
-    
+
         if ($remaining_stock > 0) {
             $total_cart_quantity = $cart_quantity + $request_quantity;
-    
+
             if ($total_cart_quantity > $remaining_stock) {
                 return [
                     'status' => false,
@@ -51,5 +54,4 @@ class Stock extends Model
             ];
         }
     }
-    
 }
