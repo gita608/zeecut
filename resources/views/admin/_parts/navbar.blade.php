3962c757e2bdd08f7d7a1cb2ef846df44f4aa5ca
Ameer Suhail <ameersuhail2001@gmail.com>
2025-05-22 01:03:43 +0530

    payment module

--- resources/views/admin/_parts/navbar.blade.php
+++ resources/views/admin/_parts/navbar.blade.php
@@ -100,7 +100,7 @@
       </li>
 
       <li class="nav-item">
-        <a href="" class="nav-link">
+        <a href="{{ route('payments.index') }}" class="nav-link">
           <i class="link-icon" data-feather="credit-card"></i>
           <span class="link-title">Payment</span>
         </a>
